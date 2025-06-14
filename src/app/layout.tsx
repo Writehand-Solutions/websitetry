21e59c1a93447955b61cd446067f06f4ad8d6140
Tinara Nathania <tinaranathania@gmail.com>
2025-06-14 18:49:34 +0700

    fix: Import grouping

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@ import localFont from 'next/font/local';
 
 import { Analytics } from '@vercel/analytics/react';
 import { SpeedInsights } from '@vercel/speed-insights/next';
-
 import type { Metadata } from 'next';
 
 import './globals.css';
